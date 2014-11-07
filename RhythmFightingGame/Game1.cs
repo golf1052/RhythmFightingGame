c27040fe6e3aea5fa49f4484c488bb789dd87fc0
Sanders Lauture <golf1052@live.com>
2014-11-07 17:19:07 -0500

    First Commit

---
+++ RhythmFightingGame/Game1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GLX;
+
+namespace RhythmFightingGame
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        GamePadState previousGamePadState = GamePad.GetState(PlayerIndex.One);
+        Player player;
+        World world;
+        GameTimeWrapper mainGameTime;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 1700;
+            graphics.PreferredBackBufferHeight = 900;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            world = new World(graphics);
+            mainGameTime = new GameTimeWrapper(MainUpdate, this, 1.0m);
+            world.AddTime(mainGameTime);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            player = new Player(Content.Load<Texture2D>("Robanimation"));
+            player.pos = new Vector2(graphics.GraphicsDevice.Viewport.Width / 2, 500);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            world.Update(gameTime);
+        }
+
+        public void MainUpdate(GameTimeWrapper gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            GamePadState gamePadState = GamePad.GetState(PlayerIndex.One);
+
+            if (gamePadState.Triggers.Left >= 0.5 && previousGamePadState.Triggers.Left < 0.5)
+            {
+                player.Dash(Player.Facing.Left);
+            }
+            if (gamePadState.Triggers.Right >= 0.5 && previousGamePadState.Triggers.Right < 0.5)
+            {
+                player.Dash(Player.Facing.Right);
+            }
+
+            player.Update(gameTime, graphics.GraphicsDevice);
+            previousGamePadState = gamePadState;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ RhythmFightingGame/Player.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using GLX;
+
+namespace RhythmFightingGame
+{
+    public class Player : Sprite
+    {
+        public enum Facing
+        {
+            Left,
+            Right
+        }
+        public Facing facing;
+
+        bool dashing;
+        Vector2 endDashPos;
+
+        public Player (Texture2D loadedTex) : base(loadedTex)
+        {
+            facing = Facing.Right;
+            dashing = false;
+        }
+
+        public void Dash(Facing direction)
+        {
+            if (!dashing)
+            {
+                dashing = true;
+                if (direction == Facing.Left)
+                {
+                    endDashPos = new Vector2(pos.X - 300, pos.Y);
+                    vel = new Vector2(-100, 0);
+                }
+                else if (direction == Facing.Right)
+                {
+                    endDashPos = new Vector2(pos.X + 300, pos.Y);
+                    vel = new Vector2(100, 0);
+                }
+            }
+        }
+
+        public override void Update(GameTimeWrapper gameTime, GraphicsDevice graphicsDevice)
+        {
+            if (dashing)
+            {
+                if (facing == Facing.Left)
+                {
+                    if (pos.X <= endDashPos.X)
+                    {
+                        vel = Vector2.Zero;
+                        dashing = false;
+                    }
+                }
+                else if (facing == Facing.Right)
+                {
+                    if (pos.X >= endDashPos.X)
+                    {
+                        vel = Vector2.Zero;
+                        dashing = false;
+                    }
+                }
+            }
+            base.Update(gameTime, graphicsDevice);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (facing == Facing.Left)
+            {
+                spriteBatch.Draw(tex, pos, null, color, rotation, origin, scale, SpriteEffects.FlipHorizontally, 0);
+            }
+            else if (facing == Facing.Right)
+            {
+                spriteBatch.Draw(tex, pos, null, color, rotation, origin, scale, SpriteEffects.None, 0);
+            }
+        }
+    }
+}
